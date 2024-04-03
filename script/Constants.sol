26c93a944a2ba55d87e4aee3c623bd8fb1ce1843
Wesley Frederickson <6827305+fredwes@users.noreply.github.com>
2024-04-02 23:03:59 -0700

    feat: config engine and risk steward

--- script/Constants.sol
+++ script/Constants.sol
@@ -3,10 +3,23 @@ pragma solidity ^0.8.10;
 
 import {IPoolAddressesProvider} from "seamless/aave-v3-core/interfaces/IPoolAddressesProvider.sol";
 import {ISequencerOracle} from "seamless/aave-v3-core/interfaces/ISequencerOracle.sol";
+import {IAaveV3ConfigEngine} from "seamless/aave-helpers/v3-config-engine/IAaveV3ConfigEngine.sol";
 
 library Constants {
     IPoolAddressesProvider constant POOL_ADDRESSES_PROVIDER =
         IPoolAddressesProvider(0x0E02EB705be325407707662C6f6d3466E939f3a0);
+
+    address constant ATOKEN_IMPLEMENTATION = 0x27076A995387458da63b23d9AFe3df851727A8dB;
+    address constant VTOKEN_IMPLEMENTATION = 0x3800DA378e17A5B8D07D0144c321163591475977;
+    address constant STOKEN_IMPLEMENTATION = 0xb4D5e163738682A955404737f88FDCF15C1391bF;
+
+    address constant REWARDS_CONTROLLER = 0x91Ac2FfF8CBeF5859eAA6DdA661feBd533cD3780;
+    address constant COLLECTOR = 0x982F3A0e3183896f9970b8A9Ea6B69Cd53AF1089;
+
+    IAaveV3ConfigEngine constant CONFIG_ENGINE = IAaveV3ConfigEngine(address(0)); // TODO
+
+    address constant GUARDIAN = 0xA1b5f2cc9B407177CD8a4ACF1699fa0b99955A22;
+
     ISequencerOracle constant SEQUENCER_ORACLE = ISequencerOracle(0xBCF85224fc0756B9Fa45aA7892530B47e10b6433);
     uint256 constant SEQUENCER_ORACLE_GRACE_PERIOD = 3600;
 }
---
+++ script/DeployConfigEngine.s.sol
@@ -0,0 +1,146 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.10;
+
+import {Script, console} from "forge-std/Script.sol";
+import {IDefaultInterestRateStrategy} from "seamless/aave-v3-core/interfaces/IDefaultInterestRateStrategy.sol";
+import {Create2Utils} from "seamless/aave-helpers/ScriptUtils.sol";
+import {AaveV3ConfigEngine} from "seamless/aave-helpers/v3-config-engine/AaveV3ConfigEngine.sol";
+import {
+    IAaveV3ConfigEngine,
+    IPool,
+    IPoolConfigurator,
+    IAaveOracle
+} from "seamless/aave-helpers/v3-config-engine/IAaveV3ConfigEngine.sol";
+import {V3RateStrategyFactory} from "seamless/aave-helpers/v3-config-engine/V3RateStrategyFactory.sol";
+import {
+    IV3RateStrategyFactory,
+    IPoolAddressesProvider
+} from "seamless/aave-helpers/v3-config-engine/IV3RateStrategyFactory.sol";
+import {ListingEngine} from "seamless/aave-helpers/v3-config-engine/libraries/ListingEngine.sol";
+import {EModeEngine} from "seamless/aave-helpers/v3-config-engine/libraries/EModeEngine.sol";
+import {BorrowEngine} from "seamless/aave-helpers/v3-config-engine/libraries/BorrowEngine.sol";
+import {CollateralEngine} from "seamless/aave-helpers/v3-config-engine/libraries/CollateralEngine.sol";
+import {PriceFeedEngine} from "seamless/aave-helpers/v3-config-engine/libraries/PriceFeedEngine.sol";
+import {RateEngine} from "seamless/aave-helpers/v3-config-engine/libraries/RateEngine.sol";
+import {CapsEngine} from "seamless/aave-helpers/v3-config-engine/libraries/CapsEngine.sol";
+import {ITransparentProxyFactory} from
+    "solidity-utils/contracts/transparent-proxy/interfaces/ITransparentProxyFactory.sol";
+import {ProxyAdmin} from "solidity-utils/contracts/transparent-proxy/ProxyAdmin.sol";
+import {TransparentProxyFactory} from "solidity-utils/contracts/transparent-proxy/TransparentProxyFactory.sol";
+import {Constants} from "./Constants.sol";
+
+contract DeployConfigEngine is Script {
+    function run() public {
+        uint256 deployerPrivateKey = vm.envUint("DEPLOYER_PRIVATE_KEY");
+        address deployerAddress = vm.addr(deployerPrivateKey);
+
+        console.log("Deployer address: ", deployerAddress);
+        console.log("Deployer balance: ", deployerAddress.balance);
+        console.log("BlockNumber: ", block.number);
+        console.log("ChainId: ", block.chainid);
+
+        vm.startBroadcast(deployerPrivateKey);
+
+        TransparentProxyFactory transparentProxyFactory = new TransparentProxyFactory();
+        ProxyAdmin proxyAdmin = ProxyAdmin(transparentProxyFactory.createProxyAdmin(deployerAddress));
+
+        (address ratesStrategyFactory,) = _createAndSetupRatesFactory(
+            IPoolAddressesProvider(address(Constants.POOL_ADDRESSES_PROVIDER)),
+            address(transparentProxyFactory),
+            address(proxyAdmin)
+        );
+
+        IAaveV3ConfigEngine.EngineLibraries memory engineLibraries = IAaveV3ConfigEngine.EngineLibraries({
+            listingEngine: Create2Utils.create2Deploy("v1", type(ListingEngine).creationCode),
+            eModeEngine: Create2Utils.create2Deploy("v1", type(EModeEngine).creationCode),
+            borrowEngine: Create2Utils.create2Deploy("v1", type(BorrowEngine).creationCode),
+            collateralEngine: Create2Utils.create2Deploy("v1", type(CollateralEngine).creationCode),
+            priceFeedEngine: Create2Utils.create2Deploy("v1", type(PriceFeedEngine).creationCode),
+            rateEngine: Create2Utils.create2Deploy("v1", type(RateEngine).creationCode),
+            capsEngine: Create2Utils.create2Deploy("v1", type(CapsEngine).creationCode)
+        });
+
+        IAaveV3ConfigEngine.EngineConstants memory engineConstants = IAaveV3ConfigEngine.EngineConstants({
+            pool: IPool(Constants.POOL_ADDRESSES_PROVIDER.getPool()),
+            poolConfigurator: IPoolConfigurator(Constants.POOL_ADDRESSES_PROVIDER.getPoolConfigurator()),
+            ratesStrategyFactory: IV3RateStrategyFactory(ratesStrategyFactory),
+            oracle: IAaveOracle(Constants.POOL_ADDRESSES_PROVIDER.getPriceOracle()),
+            rewardsController: Constants.REWARDS_CONTROLLER,
+            collector: Constants.COLLECTOR
+        });
+
+        AaveV3ConfigEngine configEngine = new AaveV3ConfigEngine(
+            Constants.ATOKEN_IMPLEMENTATION,
+            Constants.VTOKEN_IMPLEMENTATION,
+            Constants.STOKEN_IMPLEMENTATION,
+            engineConstants,
+            engineLibraries
+        );
+
+        vm.stopBroadcast();
+
+        console.log("TransparentProxyFactory deployed: ", address(transparentProxyFactory));
+        console.log("ProxyAdmin deployed: ", address(proxyAdmin));
+        console.log("V3RateStrategyFactory deployed: ", address(ratesStrategyFactory));
+        console.log("AaveV3ConfigEngine deployed: ", address(configEngine));
+
+        console.log("listingEngine deployed: ", address(engineLibraries.listingEngine));
+        console.log("eModeEngine deployed: ", address(engineLibraries.eModeEngine));
+        console.log("borrowEngine deployed: ", address(engineLibraries.borrowEngine));
+        console.log("collateralEngine deployed: ", address(engineLibraries.collateralEngine));
+        console.log("priceFeedEngine deployed: ", address(engineLibraries.priceFeedEngine));
+        console.log("rateEngine deployed: ", address(engineLibraries.rateEngine));
+        console.log("capsEngine deployed: ", address(engineLibraries.capsEngine));
+    }
+
+    function _createAndSetupRatesFactory(
+        IPoolAddressesProvider addressesProvider,
+        address transparentProxyFactory,
+        address ownerForFactory
+    ) internal returns (address, address[] memory) {
+        IDefaultInterestRateStrategy[] memory uniqueStrategies =
+            _getUniqueStrategiesOnPool(IPool(addressesProvider.getPool()));
+
+        V3RateStrategyFactory ratesFactory = V3RateStrategyFactory(
+            ITransparentProxyFactory(transparentProxyFactory).create(
+                address(new V3RateStrategyFactory(addressesProvider)),
+                ownerForFactory,
+                abi.encodeWithSelector(V3RateStrategyFactory.initialize.selector, uniqueStrategies)
+            )
+        );
+
+        address[] memory strategiesOnFactory = ratesFactory.getAllStrategies();
+
+        return (address(ratesFactory), strategiesOnFactory);
+    }
+
+    function _getUniqueStrategiesOnPool(IPool pool) internal view returns (IDefaultInterestRateStrategy[] memory) {
+        address[] memory listedAssets = pool.getReservesList();
+        IDefaultInterestRateStrategy[] memory uniqueRateStrategies =
+            new IDefaultInterestRateStrategy[](listedAssets.length);
+        uint256 uniqueRateStrategiesSize;
+        for (uint256 i = 0; i < listedAssets.length; i++) {
+            address strategy = pool.getReserveData(listedAssets[i]).interestRateStrategyAddress;
+
+            bool found;
+            for (uint256 j = 0; j < uniqueRateStrategiesSize; j++) {
+                if (strategy == address(uniqueRateStrategies[j])) {
+                    found = true;
+                    break;
+                }
+            }
+
+            if (!found) {
+                uniqueRateStrategies[uniqueRateStrategiesSize] = IDefaultInterestRateStrategy(strategy);
+                uniqueRateStrategiesSize++;
+            }
+        }
+
+        // The famous one (modify dynamic array size)
+        assembly {
+            mstore(uniqueRateStrategies, uniqueRateStrategiesSize)
+        }
+
+        return uniqueRateStrategies;
+    }
+}
