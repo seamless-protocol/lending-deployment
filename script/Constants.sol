07aca23492be222aa7b3cc33f03af99fa5447710
fredwes <6827305+fredwes@users.noreply.github.com>
2024-05-14 09:50:13 -0700

    feat: rewards controller upgrade (#6)

--- script/Constants.sol
+++ script/Constants.sol
@@ -4,6 +4,7 @@ pragma solidity ^0.8.10;
 import {IPoolAddressesProvider} from "seamless/aave-v3-core/interfaces/IPoolAddressesProvider.sol";
 import {ISequencerOracle} from "seamless/aave-v3-core/interfaces/ISequencerOracle.sol";
 import {IAaveV3ConfigEngine} from "seamless/aave-helpers/v3-config-engine/IAaveV3ConfigEngine.sol";
+import {IUiIncentiveDataProviderV3} from "seamless/aave-v3-periphery/misc/interfaces/IUiIncentiveDataProviderV3.sol";
 
 library Constants {
     IPoolAddressesProvider constant POOL_ADDRESSES_PROVIDER =
@@ -22,4 +23,11 @@ library Constants {
 
     ISequencerOracle constant SEQUENCER_ORACLE = ISequencerOracle(0xBCF85224fc0756B9Fa45aA7892530B47e10b6433);
     uint256 constant SEQUENCER_ORACLE_GRACE_PERIOD = 3600;
+
+    address constant EMISSION_MANAGER = 0x6e081F9ebb2B2f07C2f771074EBB32dDac141d14;
+
+    IUiIncentiveDataProviderV3 constant UI_INCENTIVE_PROVIDER_V3 =
+        IUiIncentiveDataProviderV3(0x3F5a90eF7BC3eE64e1E95b850DbBC2469fF71ce8);
+
+    address constant TIMELOCK_SHORT = 0x639d2dD24304aC2e6A691d8c1cFf4a2665925fee;
 }
