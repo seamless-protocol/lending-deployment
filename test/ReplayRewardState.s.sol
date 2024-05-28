b1f84a79ccf4fa789e82b33077c6d69945697b6f
Wesley Frederickson <6827305+fredwes@users.noreply.github.com>
2024-05-28 15:08:53 -0700

    replay state script

--- test/ReplayRewardState.s.sol
+++ test/ReplayRewardState.s.sol
@@ -3,6 +3,7 @@ pragma solidity ^0.8.10;
 
 import {Test, console} from "forge-std/Test.sol";
 import "forge-std/StdJson.sol";
+import {Strings} from "@openzeppelin/contracts/utils/Strings.sol";
 import {IERC20} from "forge-std/interfaces/IERC20.sol";
 import {InitializableImmutableAdminUpgradeabilityProxy} from
     "seamless/aave-v3-core/protocol/libraries/aave-upgradeability/InitializableImmutableAdminUpgradeabilityProxy.sol";
@@ -23,63 +24,73 @@ contract ReplayRewardState is Test {
     bytes32 constant REWARDS_PROXY_ADDRESS_ID = keccak256("INCENTIVES_CONTROLLER");
     address constant controllerImplementationV2 = 0x8243De25c4B8a2fF57F38f89f7C989F7d0fc2850;
 
-    address constant user = 0x99e5d4a7Fb7BA7281D1F4fc5DCE311F1d832796C;
-
-    address constant USDC = 0x833589fCD6eDb6E08f4c7C32D4f71b54bdA02913;
-
-    address usdcAToken;
+    string constant outputFileName = "./test/output.csv";
 
     RewardsController rewardsProxy;
+    address[] assetsList;
 
     function setUp() public {
         vm.createSelectFork(vm.envString("FORK_URL"), FORK_BLOCK_NUMBER - 1);
 
-        IPool pool = IPool(Constants.POOL_ADDRESSES_PROVIDER.getPool());
-        DataTypes.ReserveData memory usdcReserve = pool.getReserveData(USDC);
-
-        usdcAToken = usdcReserve.aTokenAddress;
-
         rewardsProxy = RewardsController(Constants.POOL_ADDRESSES_PROVIDER.getAddress(REWARDS_PROXY_ADDRESS_ID));
+
+        IUiIncentiveDataProviderV3.AggregatedReserveIncentiveData[] memory incentiveData = Constants
+            .UI_INCENTIVE_PROVIDER_V3
+            .getReservesIncentivesData(IPoolAddressesProvider(address(Constants.POOL_ADDRESSES_PROVIDER)));
+
+        for (uint256 i = 0; i < incentiveData.length; i++) {
+            assetsList.push(incentiveData[i].aIncentiveData.tokenAddress);
+            assetsList.push(incentiveData[i].vIncentiveData.tokenAddress);
+        }
 
         address controllerImplementationV3 = address(new RewardsController(Constants.EMISSION_MANAGER));
         vm.etch(controllerImplementationV2, controllerImplementationV3.code);
     }
 
     function test_run() public {
+        // bytes32 afterTimerecentTxlockTx = 0x4e5f725376c58b9f710dd2ece16ca10175d8f52d535996455ebce7e7a2d77c51;
+        bytes32 recentTx = 0xf547f5e520552750c24c74e084bfb7163e37875c1c260db0224f26b879716911;
         vm.makePersistent(address(controllerImplementationV2));
 
-        bytes32 afterBadClaimTx = 0x1e2c8687937673c88f382f7b645e9ff4e424464ed81607ab7a4e72b83e692228;
-        vm.rollFork(afterBadClaimTx);
+        vm.rollFork(recentTx);
+
+        console.log("Fork rolled forward");
 
         assertEq(rewardsProxy.REVISION(), 3);
 
-        uint256 userIndex = rewardsProxy.getUserAssetIndex(user, usdcAToken, USDC);
-        assertNotEq(userIndex, 0);
+        _writeState();
+    }
 
+    function _writeState() internal {
         // {
-        //     "values": [
-        //         {
-        //             "asset": "string",
-        //             "reward": "string",
-        //             "user": "string",
-        //         }
-        //     ]
+        //     "users": ["address_string"]
         // }
-        string memory json = vm.readFile("./replay-reward-state-input.json");
+        string memory usersJson = vm.readFile("./test/users.json");
 
-        uint256 count;
-        while(true) {
-            address asset ;
+        address[] memory users = usersJson.readAddressArray(".users");
+        if (vm.exists(outputFileName)) {
+            vm.removeFile(outputFileName);
+        }
 
-            if (asset != address(0)) {
-                break;
+        console.log("Json parsed.");
+
+        console.log("Start fetching user state.");
+
+        vm.writeLine(outputFileName, "asset,reward,user,userAccrued,userIndex");
+
+        for (uint256 i = 0; i < assetsList.length; i++) {
+            address[] memory rewards = rewardsProxy.getRewardsByAsset(assetsList[i]);
+
+            for (uint256 j = 0; j < rewards.length; j++) {
+                for (uint256 k = 0; k < users.length; k++) {
+                    (uint256 userAccrued, uint256 userIndex) = rewardsProxy.getUserAssetData(users[k], assetsList[i], rewards[j]);
+                    // console.log("asset %s, reward %s, user %s", assetsList[i], rewards[j], users[k]);
+                    // console.log("userAccrued %s, userIndex %s", userAccrued, userIndex);
+                    if (userAccrued != 0 || userIndex != 0) {
+                        vm.writeLine(outputFileName, string.concat(Strings.toHexString(assetsList[i]), ",", Strings.toHexString(rewards[j]), ",", Strings.toHexString(users[k]), ",", Strings.toString(userAccrued), ",", Strings.toString(userIndex)));
+                    }
+                }
             }
-
-            address reward;
-            address user;
-            
-            
-            count++;
         }
     }
 }
